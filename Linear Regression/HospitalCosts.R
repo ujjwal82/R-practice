b893394bb3c35172ab27f3806210fbffacfae87a
Ujjwal Kumar <ujjwal82@gmail.com>
2017-12-10 00:03:49 +0530

    Revised model - 1

--- Linear Regression/HospitalCosts.R
+++ Linear Regression/HospitalCosts.R
@@ -3,22 +3,51 @@
 # Importing the dataset
 dataset = read.csv('HospitalCosts.csv')
 
+dataset$APRDRG <- factor(dataset$APRDRG)
+table(dataset$APRDRG)
+
+
+## ------------------------------------------
+
+aa <- subset(x = dataset, APRDRG == 640)
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$TOTCHG, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+split = sample.split(aa$TOTCHG, SplitRatio = 0.8)
+training_set = subset(aa, split == TRUE)
+test_set = subset(aa, split == FALSE)
 
-# Feature Scaling
-#training_set = scale(training_set[c(-5,-6)])
-#test_set = scale(test_set[c(-5,-6)])
+model_640 <- lm(formula = TOTCHG ~ AGE + FEMALE + LOS + RACE, data = training_set)
+summary(model_640)
 
-regressor <- lm(formula = TOTCHG ~ ., data = training_set)
-y_pred <- predict(regressor, test_set)
+model_640 <- lm(formula = TOTCHG ~ AGE + FEMALE + LOS, data = training_set)
+summary(model_640)
+y_pred <- predict(model_640, test_set)
 
-summary(regressor)
+## ---------------------------
+
+aa <- subset(x = dataset, APRDRG == 754)
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(aa$TOTCHG, SplitRatio = 0.8)
+training_set = subset(aa, split == TRUE)
+test_set = subset(aa, split == FALSE)
+
+model_754 <- lm(formula = TOTCHG ~ AGE + FEMALE + LOS + RACE, data = training_set)
+summary(model_754)
+
+model_754 <- lm(formula = TOTCHG ~ AGE + LOS + RACE, data = training_set)
+summary(model_754)
+y_pred <- predict(model_754, test_set)
+
+## ---------------------------
+
+
 
 
 df <- as.data.frame(c(y_pred, test_set[5]))
@@ -29,3 +58,6 @@ df <- data.frame(y_pred,  test_set$TOTCHG)
 
 colnames(df) <- c('Predited', 'Actual')
 
+##
+unique( dataset$APRDRG)
+table( dataset$APRDRG)
