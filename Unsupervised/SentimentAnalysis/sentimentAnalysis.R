7875735fe6933dc1abb79518308bcbd4eefde08a
Ujjwal Kumar <ujjwal82@gmail.com>
2017-12-21 23:55:01 +0530

    Added more charts

--- Unsupervised/SentimentAnalysis/sentimentAnalysis.R
+++ Unsupervised/SentimentAnalysis/sentimentAnalysis.R
@@ -31,7 +31,11 @@ library(gridExtra)
 
 
 
+
 SentimentAnalysis <- function(textFileName, title) {
+  # textFileName <- 'Obama-SuperTuesday_5_Feb_2008.txt'
+  # title <- 'Super Tuesday (5 Feb 2008) Sentiment'
+  
   text <- paste(readLines(textFileName), collapse = " ")
   ###
   # Remove the stop words from the speach text
@@ -58,6 +62,10 @@ SentimentAnalysis <- function(textFileName, title) {
   ###
   mySent <- get_nrc_sentiment(text)
 
+  rownames(mySent) <- sub("\\).*", "", sub(".*\\(", "", title)) 
+
+  Comp_sent <<- rbind(Comp_sent, mySent, stringsAsFactors=FALSE)
+  
   ###
   # Now lets draw the table of sentiment break-ups
   ###
@@ -95,10 +103,10 @@ speeachList$textFileName <- c(
   'Obama-InauguralAddress_20_Jan_2009.txt')
 
 speeachList$barplotTitle <- c(
-  'Super Tuesday (5 Feb 2008) Sentiment',
-  'Potomac Primary Night (12 Feb 2008) Sentiment',
-  'Texas And Ohio Primary Night (4 Mar 2008) Sentiment',
-  'AP Annual Luncheon (14 Apr 2008) Sentiment',
+  'Super Tuesday (5 Feb\'08) Sentiment',
+  'Potomac Primary Night (12 Feb\'08) Sentiment',
+  'Texas And Ohio Primary Night (4 Mar\'08) Sentiment',
+  'AP Annual Luncheon (14 Apr\'08) Sentiment',
   'Pennsylvania Primary Night (22 Apr 2008) Sentiment',
   'North Carolina Primary Night (6 May 2008) Sentiment',
   'Presumptive Democratic Nominee (2 Jun 2008) Sentiment',
@@ -113,6 +121,22 @@ pdf(file = somePDFPath, onefile = T, paper = 'A4r', width = 5 * 2)
 
 # par(mfrow = c(2, 1))
 layout(matrix(c(1,2), nrow = 2, ncol = 1, byrow = TRUE))
+# par(mai=c(0,1,0,1))
+
+###
+# Lets create a dataframe to compare the sentiments across the speeches
+###
+Comp_sent <- data.frame(anger = integer(), 
+                        anticipation = integer(),
+                        disgust = integer(),
+                        fear = integer(),
+                        joy = integer(),
+                        sadness = integer(),
+                        surprise = integer(),
+                        trust = integer(),
+                        negative = integer(), 
+                        positive = integer()
+                        )
 
 
 for (i in 1:length(speeachList$textFileName)){
@@ -120,9 +144,40 @@ for (i in 1:length(speeachList$textFileName)){
   
 }
 
+
+###
+# Now let's draw the table of sentiment break-ups
+###
+layout(mat = matrix(c(1), nrow = 1, ncol = 1, byrow = TRUE))
+plot.new()
+par(mai=c(1,1,0,0))
+vps <- baseViewports()
+pushViewport(vps$inner, vps$figure, vps$plot)
+grob <-  tableGrob(Comp_sent)  
+grid.draw(grob)
+popViewport(3)
+
+###
+# Plot each emotion separatly, 4 plots each page
+###
+layout(mat = matrix(c(1,2, 3,4), nrow = 2, ncol = 2, byrow = TRUE))
+
+par(mai=c(1,1,0,0))
+par(las = 1)
+
+
+barplot(Comp_sent$anger, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Anger')
+barplot(Comp_sent$anticipation, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Anticipation')
+barplot(Comp_sent$disgust, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Disgust')
+barplot(Comp_sent$fear, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Fear')
+barplot(Comp_sent$joy, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Joy')
+barplot(Comp_sent$sadness, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Sadness')
+barplot(Comp_sent$surprise, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Surprise')
+barplot(Comp_sent$trust, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Trust')
+barplot(Comp_sent$negative, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Negative')
+barplot(Comp_sent$positive, names.arg =  rownames(Comp_sent), horiz = TRUE, axes = TRUE, xlab = 'Positive')
 dev.off() 
 
 ## ---------------------
 
 # Reference URL: https://stackoverflow.com/questions/25192838/arrange-base-plots-and-grid-tables-on-the-same-page
-
