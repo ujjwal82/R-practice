bb5417737ff18a31c8c39b02ce8ce6f570c842ac
Ujjwal Kumar <Ujjwal.Kumar@fisglobal.com>
2017-12-21 19:00:06 +0530

    More data for sentiment analysis

--- Unsupervised/SentimentAnalysis/sentimentAnalysis.R
+++ Unsupervised/SentimentAnalysis/sentimentAnalysis.R
@@ -1,4 +1,7 @@
-# Sentiment analysis of Obama's different speeches
+## -----------------------------------------------------##
+# Sentiment analysis of Obama's different speeches       #
+# Source of text speeches: http://obamaspeeches.com/     #
+## -----------------------------------------------------##
 
 # install.packages("SnowballC")
 # install.packages('tm')
@@ -8,6 +11,8 @@
 # install.packages('syuzhet')
 # install.packages('plyr')
 # install.packages('sentimentr')
+# install.packages('grid')
+# install.packages('gridBase')
 # install.packages('gridExtra')
 
 library(NLP)
@@ -19,7 +24,12 @@ library(wordcloud)
 library(syuzhet)
 library(plyr)
 library(sentimentr)
+
+library(grid)
+library(gridBase)
 library(gridExtra)
+
+
 
 SentimentAnalysis <- function(textFileName, title) {
   text <- paste(readLines(textFileName), collapse = " ")
@@ -42,13 +52,25 @@ SentimentAnalysis <- function(textFileName, title) {
   # Now we have the word cloud
   ###
   # wordcloud(words = bag_of_word, min.freq = 5, random.order = FALSE, random.color =FALSE)
-  
-  
+
+  ###
+  # get the sentiment breakups
+  ###
   mySent <- get_nrc_sentiment(text)
-  mySent
-  colnames(mySent)
-  
-  grid.table(mySent,  cols = colnames(mySent))
+
+  ###
+  # Now lets draw the table of sentiment break-ups
+  ###
+  plot.new()
+  vps <- baseViewports()
+  pushViewport(vps$inner, vps$figure, vps$plot)
+  grob <-  tableGrob(mySent)  
+  grid.draw(grob)
+  popViewport(3)
+
+  ###
+  # Draw bar chart of sentiment break-ups
+  ###
   barplot(
     as.matrix(mySent),
     main = title,
@@ -58,47 +80,49 @@ SentimentAnalysis <- function(textFileName, title) {
   )
 }
 
+speeachList <- list()
+speeachList$textFileName <- c(
+  'Obama-SuperTuesday_5_Feb_2008.txt',
+  'Obama-PotomacPrimaryNight_12_Feb_2008.txt',
+  'Obama-TexasAndOhioPrimaryNight_4_Mar_2008.txt',
+  'Obama-AP Annual Luncheon_14_Apr_2008.txt',
+  'Obama-PennsylvaniaPrimaryNight_22_Apr_2008.txt',
+  'Obama-NorthCarolinaPrimaryNight_6_May_2008.txt',
+  'Obama-PresumptiveDemocraticNomineeSpeech_2_Jun_2008.txt',
+  'Obama-NomineeAcceptance_28_Aug_2008.txt',
+  'Obama-NightBeforeElection_3_Nov_2008.txt',
+  'Obama-ElectionNightVictorySpeech_4_Nov_2008.txt', 
+  'Obama-InauguralAddress_20_Jan_2009.txt')
+
+speeachList$barplotTitle <- c(
+  'Super Tuesday (5 Feb 2008) Sentiment',
+  'Potomac Primary Night (12 Feb 2008) Sentiment',
+  'Texas And Ohio Primary Night (4 Mar 2008) Sentiment',
+  'AP Annual Luncheon (14 Apr 2008) Sentiment',
+  'Pennsylvania Primary Night (22 Apr 2008) Sentiment',
+  'North Carolina Primary Night (6 May 2008) Sentiment',
+  'Presumptive Democratic Nominee (2 Jun 2008) Sentiment',
+  'Nomination Acceptance (28 Aug 2008) Sentiment',
+  'Night Before Election (3 Nov 2008) Sentiment',
+  'Election Night Victory Speech (04 Nov 2008) Sentiment',
+  'Inaugural Address Speech (20 Jan 2009) Sentiment')
+
+
 somePDFPath = "exportReport.pdf"
-pdf(
-  file = somePDFPath,
-  onefile = T,
-  paper = 'A4r',
-  width = 5 * 2
-)
+pdf(file = somePDFPath, onefile = T, paper = 'A4r', width = 5 * 2)
 
-par(mfrow = c(2, 2))
-textFileName <- 'Obama-ElectionNightVictorySpeech_4_Nov_2008.txt'
-barplotTitle <-
-  "Election Night Victory Speech (04 Nov 2008) Sentiment"
-SentimentAnalysis(textFileName, barplotTitle)
-
-textFileName <- 'Obama-InauguralAddress_20_Jan_2009.txt'
-barplotTitle <- "Inaugural Address Speech (20 Jn 2009) Sentiment"
-SentimentAnalysis(textFileName, barplotTitle)
+# par(mfrow = c(2, 1))
+layout(matrix(c(1,2), nrow = 2, ncol = 1, byrow = TRUE))
 
 
-library(gridExtra)
-d <- head(iris)
-table <- tableGrob(d)
+for (i in 1:length(speeachList$textFileName)){
+  SentimentAnalysis(speeachList$textFileName[i], speeachList$barplotTitle[i])
+  
+}
 
-library(grid)
-library(gtable)
-
-title <- textGrob("Title",gp=gpar(fontsize=50))
-footnote <- textGrob("footnote", x=0, hjust=0,
-                     gp=gpar( fontface="italic"))
-
-padding <- unit(0.5,"line")
-table <- gtable_add_rows(table, 
-                         heights = grobHeight(title) + padding,
-                         pos = 0)
-table <- gtable_add_rows(table, 
-                         heights = grobHeight(footnote)+ padding)
-table <- gtable_add_grob(table, list(title, footnote),
-                         t=c(1, nrow(table)), l=c(1,2), 
-                         r=ncol(table))
-grid.newpage()
-grid.draw(table)
 dev.off() 
 
 ## ---------------------
+
+# Reference URL: https://stackoverflow.com/questions/25192838/arrange-base-plots-and-grid-tables-on-the-same-page
+
