31171f7ec3ec40c6be371b6c55178724969924f8
Ujjwal Kumar <ujjwal82@gmail.com>
2017-12-28 23:02:13 +0530

    progressive commit.

--- NeuralNetworks/Boston.R
+++ NeuralNetworks/Boston.R
@@ -6,17 +6,10 @@ library(neuralnet)
 set.seed(123)
 dataset <- Boston
 
-# Structure of the dataset
-str(dataset)
-
 # Histogram of the medv
 hist(dataset$medv)
 
-
-maxValue <- apply(dataset, 2, max)
-minValue <- apply(dataset, 2, min)
-
-dataset <- as.data.frame(scale(dataset,center = minValue, scale = maxValue))
+#dataset <- as.data.frame(scale(dataset,center = minValue, scale = maxValue))
 
 # Splitting the dataset into training set and test set
 library(caTools)
@@ -25,6 +18,8 @@ split <- sample.split(dataset$medv, SplitRatio = 0.8)
 training_set <- subset(dataset, split == TRUE)
 test_set <- subset(dataset, split == FALSE)
 
+training_set[-14] <- as.data.frame(scale(training_set[-14]))
+test_set[-14] <- as.data.frame(scale(training_set[-14]))
 
 # Fitting the NeuralNet model
 
@@ -44,4 +39,18 @@ neuralModel <- neuralnet(formula = formula, hidden = c(4, 2),
                          data = training_set
                          )
 
+# Visulazing the model
 plot(neuralModel)
+
+# Predicting the test set results
+predictions <- compute(neuralModel, test_set[, 1:13])
+
+predictions <- predictions$net.result
+actualValues <- test_set[14]
+
+MSE <- sum((predictions - actualValues)^2)/nrow(test_set)
+MSE
+
+plot(test_set$medv, predictions, col = 'blue', main = "Real vs Predicted",
+     pch = 1, cex = 0.9, type ='p')
+
