35eca9c9fab32e14e201148391f95312c33d068c
Ujjwal Kumar <ujjwal82@gmail.com>
2018-03-25 03:04:42 +0530

    second draft and updated with some comments and one visualization function

--- Wages Problem/Wages.R
+++ Wages Problem/Wages.R
@@ -5,35 +5,67 @@ setwd("D:/ujjwal/Tutorial/DataScience/R-practice/Wages Problem")
 ###
 # Get the data in to R
 ###
-dataset = read.csv('data/Wage_1', stringsAsFactors = FALSE)
+dataset_raw = read.csv('data/Wage_1', stringsAsFactors = FALSE)
 
-colnames(dataset)
+## Take a copy of the original data
+dataset <- dataset_raw
+
+## Remove any leadign spaces from all the columns (apply only to character values columns)
+dataset[sapply(dataset, is.character)] <- data.frame(lapply(dataset[sapply(dataset, is.character)], trimws), stringsAsFactors = TRUE)
+
+## Now convert all the factors (converted in factors in previous step) into numeric
+dataset[sapply(dataset, is.factor)] <- data.frame(lapply(dataset[sapply(dataset, is.factor)], as.numeric))
+
+## Now convert all columns to factor, except last two columns (lowwages and wages, which will be our dependent variables)
+dataset[1:(ncol(dataset)-2)] <- lapply(dataset[1:(ncol(dataset)-2)], as.factor)
+
+###
+# Step #1 to 4 can be applied to any dataset, once we have removed all missing values
+# Step #5 is specific to problem, need to apply with index of dependent variable.
+###
 summary(dataset)
-str(dataset)
+
+plot(dataset$wage)
+hist(x = dataset$wage)
+library(plotly)
 
 
-dataset <- data.frame(lapply(dataset, trimws), stringsAsFactors = TRUE)
+###
+# Visualizing data
+###
 
 
-dataset <- dataset %>%
-  mutate(jobclass = ifelse(jobclass == "Industrial", 1, 2),
-         race = ifelse(race == 'Asian', 1, ifelse(race == 'Black',2,3)),
-         sex = ifelse(sex == 'Male', 1, 2))
+df <- dataset %>%
+  filter(year == 2004, race == 1) %>%
+  group_by(age, maritl) %>%
+  select(age, maritl, wage)
 
-head(dataset)
+df %>%
+  plot_ly()%>%
+  add_trace(x = ~age, y = ~wage, type = 'bar',
+            text = df$wage, textposition = 'auto') %>%
+  # add_trace(x = ~maritl, y = ~wage, type = 'bar',
+  #           text = df$wage, textposition = 'auto') %>%
+  layout(title = "Wages grouped by Age and Marital Status(Year 2004)",
+         barmode = 'group',
+         xaxis = list(title = "Month"),
+         yaxis = list(title = "Wages"))
 
-dataset$sex <- trimws(dataset$sex)
+
+###
+# Split data
+###
+set.seed(54321)
+indexes <- createDataPartition( dataset$wage,
+                                times = 1,
+                                p = 0.7,
+                                list = FALSE)
+dataset.train <- dataset[indexes,]
+dataset.test <-  dataset[-indexes,]
 
 
-1:length(levels(dataset$maritl))
-as.numeric(dataset$maritl)
 
 
-dataset$maritl <- trimws(dataset$maritl)
-dataset$race <- trimws(dataset$race) 
-dataset$education <- trimws(dataset$education)
-dataset$region <- trimws(dataset$region)
-dataset$health <- trimws(dataset$health)
-dataset$health_ins <- trimws(dataset$health_ins)
-dataset$jobclass <- trimws(dataset$jobclass) 
+
+
 
